93fcb77de83be513e2d18329231fbc9f91b96587
lessless <yevhenii.kurtov@gmail.com>
2017-09-07 19:51:49 +0700

    Update Request.Url tests

--- lib/ex_monero/request/url.ex
+++ lib/ex_monero/request/url.ex
@@ -7,33 +7,12 @@ defmodule ExMonero.Request.Url do
   def build(operation, config) do
     URI.parse(config.url)
     |> Map.put(:path, operation.path)
+    |> normalize_path()
     |> URI.to_string()
     |> String.trim_trailing("?")
-  end
-
-  defp query(operation) do
-    operation
-    |> Map.get(:params, %{})
-    |> normalize_params
-    |> URI.encode_query
-  end
-
-  defp normalize_scheme(url) do
-    url |> Map.update(:scheme, "", &String.replace(&1, "://", ""))
   end
 
   defp normalize_path(url) do
     url |> Map.update(:path, "", &String.replace(&1, ~r/\/{2,}/, "/"))
   end
-
-  defp convert_port_to_integer(url = %{port: port}) when is_binary(port) do
-    {port, _} = Integer.parse(port)
-    put_in(url[:port], port)
-  end
-  defp convert_port_to_integer(url), do: url
-
-  defp normalize_params(params) when is_map(params)  do
-    params |> Map.delete("") |> Map.delete(nil)
-  end
-  defp normalize_params(params), do: params
 end
