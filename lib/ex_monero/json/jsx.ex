c77e6eae70008c2696e5e9a244251be6732eee2c
lessless <yevhenii.kurtov@gmail.com>
2017-09-08 11:39:34 +0700

    Add JSON.Codec behavior

---
+++ lib/ex_monero/json/jsx.ex
@@ -0,0 +1,39 @@
+defmodule ExMonero.JSON.JSX do
+  @behaviour ExMonero.JSON.Codec
+
+  @moduledoc false
+
+  if Code.ensure_loaded?(:jsx) do
+    def encode!(%{} = map) do
+      map |> :jsx.encode
+    end
+
+    def decode!(string) do
+      :jsx.decode(string, [:return_maps])
+    end
+  else
+    def encode!(_) do
+      raise ":jsx must be added as a dependency to use this module"
+    end
+
+    def decode!(_) do
+      raise ":jsx must be added as a dependency to use this module"
+    end
+  end
+
+  def encode(map) do
+    try do
+      {:ok, encode!(map)}
+    rescue
+      ArgumentError -> {:error, :badarg}
+    end
+  end
+
+  def decode(string) do
+    try do
+      {:ok, decode!(string)}
+    rescue
+      ArgumentError -> {:error, :badarg}
+    end
+  end
+end
