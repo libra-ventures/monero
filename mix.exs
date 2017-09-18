0599b1fd3f5de3b6084b807e0f43d9411857f661
Andrey N. Ronin <anronin@gmail.com>
2017-09-18 19:36:11 +0400

    Fix some credo warnings

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule Monero.Mixfile do
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib",]
+  defp elixirc_paths(_),     do: ["lib"]
 
   defp deps do
     [
