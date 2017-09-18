7ba5dd8e579d7339cba09a1fd1cbda19dcc79e1a
Andrey N. Ronin <anronin@gmail.com>
2017-09-18 19:01:50 +0400

    Add several most needed wallet rpc methods

--- test/lib/monero/wallet_test.exs
+++ test/lib/monero/wallet_test.exs
@@ -6,8 +6,36 @@ defmodule Monero.WalletTest do
     assert %{path: "/json_rpc", service: :wallet} = Wallet.getbalance()
   end
 
-  test "#getbalance" do
+  test "getbalance/0" do
     expected = %{jsonrpc: "2.0", method: "getbalance", params: nil}
     assert expected == Wallet.getbalance().data
+  end
+
+  test "getaddress/0" do
+    expected = %{jsonrpc: "2.0", method: "getaddress", params: nil}
+    assert expected == Wallet.getaddress().data
+  end
+
+  test "get_payments/1" do
+    params = %{payment_id: "0000000000000000"}
+    expected = %{jsonrpc: "2.0", method: "get_payments", params: params}
+    assert expected == Wallet.get_payments("0000000000000000").data
+  end
+
+  test "incoming_transfers/1" do
+    expected = %{jsonrpc: "2.0", method: "incoming_transfers", params: %{transfer_type: "all"}}
+    assert expected == Wallet.incoming_transfers("all").data
+  end
+
+  test "create_wallet/3" do
+    params = %{filename: "test-wallet", password: "password", language: "English"}
+    expected = %{jsonrpc: "2.0", method: "create_wallet", params: params}
+    assert expected == Wallet.create_wallet("test-wallet", "password", "English").data
+  end
+
+  test "open_wallet/3" do
+    params = %{filename: "test-wallet", password: "password"}
+    expected = %{jsonrpc: "2.0", method: "open_wallet", params: params}
+    assert expected == Wallet.open_wallet("test-wallet", "password").data
   end
 end
